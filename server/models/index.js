7b02ab6a4cbf73da411698e0868f4af6fd5478b5
fredkk22 <freddykwak123@gmail.com>
2022-09-27 12:58:40 -0400

    Got rid of unnecessary model

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,3 @@
-const Student = require('./Student');
-const Course = require('./Course');
 const User = require('./User');
 
-module.exports = { Student, Course, User };
+module.exports = { User };
