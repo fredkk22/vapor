4fc49ac0177017a606dbd59aae6d080ded68abf9
fredkk22 <freddykwak123@gmail.com>
2022-09-27 14:03:48 -0400

    Added schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,143 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Game, Genre, Order } = require('../models');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+const resolvers = {
+  Query: {
+    genres: async () => {
+      return await Genre.find();
+    },
+    games: async (parent, { genre, title }) => {
+      const params = {};
+
+      if (genre) {
+        params.genre = genre;
+      }
+
+      if (title) {
+        params.title = {
+          $regex: title
+        };
+      }
+
+      return await Game.find(params).populate('genres');
+    },
+    game: async (parent, { _id }) => {
+      return await Game.findById(_id).populate('genres');
+    },
+    user: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate({
+          path: 'orders.games',
+          populate: 'genres'
+        });
+
+        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+
+        return user;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    order: async (parent, { _id }, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate({
+          path: 'orders.games',
+          populate: 'genres'
+        });
+
+        return user.orders.id(_id);
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ games: args.games });
+      const line_items = [];
+
+      const { games } = await order.populate('games');
+
+      for (let i = 0; i < games.length; i++) {
+        const game = await stripe.games.create({
+          title: games[i].name,
+          description: games[i].description,
+          images: [`${url}/images/${games[i].image}`]
+        });
+
+        const price = await stripe.prices.create({
+          game: game.id,
+          unit_amount: games[i].price * 100,
+          currency: 'usd',
+        });
+
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        });
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`
+      });
+
+      return { session: session.id };
+    }
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addOrder: async (parent, { games }, context) => {
+      console.log(context);
+      if (context.user) {
+        const order = new Order({ games });
+
+        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
+
+        return order;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    updateGame: async (parent, { _id, quantity }) => {
+      const decrement = Math.abs(quantity) * -1;
+
+      return await Game.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true });
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    }
+  }
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,60 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Genre {
+    _id: ID
+    name: String
+  }
+
+  type Game {
+    _id: ID
+    title: String
+    description: String
+    image: String
+    price: Float
+    quantity: Int
+    genres: [Genre]
+  }
+
+  type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+  }
+
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
+
+  type Checkout {
+    session: ID
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    genres: [Genre]
+    games(genre: ID, title: String): [Game]
+    game(_id: ID!): Game
+    user: User
+    order(_id: ID!): Order
+    checkout(games: [ID]!): Checkout
+  }
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addOrder(games: [ID]!): Order
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateGame(_id: ID!, quantity: Int!): Game
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
