257d6e0ed1532632f92e1294314408d341e68abd
christophermowrey <christophermowrey@gmail.com>
2022-09-28 10:29:04 -0400

    Updated Client folder to finished

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,18 +1,7 @@
-// TODO: Add a comment briefly describing the functionality of `useContext`
-// Your comment here
-
-import React, { createContext, useContext } from 'react';
-
-// TODO: Add a comment describing the React hook that `useProductReducer` makes use of
-// Your comment here
-
-import { useProductReducer } from './reducers';
-
-// TODO: Add a comment briefly describing what `createContext` does and what it returns
-// Your comment here
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers'
 
 const StoreContext = createContext();
-
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
@@ -24,13 +13,9 @@ const StoreProvider = ({ value = [], ...props }) => {
     currentCategory: '',
   });
 
-  // TODO: Add a comment explaining why we passed a `value` attribute to the `Provider`
-  // Your comment here
   return <Provider value={[state, dispatch]} {...props} />;
 };
 
-// TODO: Add a comment describing the purpose of our custom `useStoreContext` hook
-// Your comment here
 const useStoreContext = () => {
   return useContext(StoreContext);
 };
