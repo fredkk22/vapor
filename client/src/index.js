bda4562560870dc62187307c6463b5a78859477a
aaansari123 <asadali1029@gmail.com>
2022-09-29 13:31:37 -0400

    pushing working manifest and trial service worker

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker.js';
+import * as serviceWorker from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
