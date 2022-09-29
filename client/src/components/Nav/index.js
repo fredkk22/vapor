4251d6845b35e99af503aed7eae7eafc787dffb4
christophermowrey <christophermowrey@gmail.com>
2022-09-29 13:06:02 -0400

    navbar adjustment

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -23,30 +23,45 @@ function Nav() {
       );
     } else {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to='/signup'>
-              Signup
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to='/login'>
-              Login
-            </Link>
-          </li>
-        </ul>
+        <div>
+      
+    
+        <div class="b-example-divider"></div>
+      
+        <header class="p-3 text-bg-dark">
+          <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+              <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlinkHref="#bootstrap"></use></svg>
+              </a>
+      
+              <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+              <Link to="/">
+              <button type="button" class="btn btn-outline-light me-2">HOME</button>
+              </Link>
+              <Link to="/Subscription">
+              <button type="button" class="btn btn-outline-light">SUBSCRIBE</button>
+                </Link>
+              </ul>
+  
+      
+              <div class="text-end">
+                
+                <button type="button" class="btn btn-outline-light me-2">Login</button>
+                <button type="button" class="btn btn-warning">Sign-up</button>
+              </div>
+            </div>
+          </div>
+        </header>
+      
+        
+      </div>
       );
     }
   }
 
   return (
     <header className="flex-row px-1">
-      <h1>
-        <Link to='/'>
-           VAPOR
-        </Link>
-      </h1>
-
       <nav>
         {showNavigation()}
       </nav>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
             <h1 class="fw-light">Album example</h1>
             <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
             <p>
-              <a href="#" class="btn btn-primary my-2">SUBSCRIBE TO VAPOR ELITE PRO</a>
+              <a href="#" class="btn btn-primary my-2">SUBSCRIBE TO VAPOR</a>
             </p>
           </div>
         </div>
