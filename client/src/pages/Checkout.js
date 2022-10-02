e194fd1a03547ee5196c77b6161801b3bdc2d374
zym0tic <lukerozon@gmail.com>
2022-10-02 19:56:34 -0400

    deploying to heroku

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -8,9 +8,7 @@ const Checkout = () => {
     <div className="container">
     <main>
       <div className="py-5 text-center">
-        <img className="d-block mx-auto mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"></img>
-        <h2>Checkout form</h2>
-        <p className="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+        <h2>Checkout</h2>
       </div>
   
       <div className="row g-5">
